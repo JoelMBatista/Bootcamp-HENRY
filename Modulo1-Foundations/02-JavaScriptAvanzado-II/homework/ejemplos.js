56cce4396d130040c81a5a0408b05db6580afeee
Kapelu <ddaniel_calderon@hotmail.com>
2022-05-08 19:57:54 -0300

    Update 08/05/22

--- Modulo1-Foundations/02-JavaScriptAvanzado-II/homework/ejemplos.js
+++ Modulo1-Foundations/02-JavaScriptAvanzado-II/homework/ejemplos.js
@@ -1,7 +1,6 @@
 /* 
-  $CLOUSURE: Es una funcion que encapsula una serie de variables y definiciones locales que solo seran accecibles si son devueltas con `return`
+$ CLOUSURE: Es una funcion que encapsula una serie de variables y definiciones locales que solo seran accecibles si son devueltas con `return`
 */
-
 function saludar(saludo) {
   return function (nombre) {
     return saludo + ' ' + nombre
@@ -38,6 +37,41 @@ function global(){
 
 const closure=global()
 console.log(closure())
+
+//********************************************************* */
+function producto(a){return (b)=> a * b}
+
+var tabla = producto(2)
+
+for (let i = 0; i < 11; i++) {
+  console.log(tabla(i));
+} 
+//********************************************************* */
+
+var creaFuncion = function(){
+  var arreglo = [];
+
+  for ( var i=0; i < 3; i++){
+    arreglo.push(
+      function(){
+        console.log(i);
+      }
+    )
+  }
+  return arreglo;
+}
+
+var arr = creaFuncion();
+
+arr[0]() // 3 sale un 3, qué esperaban ustedes??
+arr[2]() // 3
+arr[1]() // 3
+
+
+
+
+
+
 
 
 
