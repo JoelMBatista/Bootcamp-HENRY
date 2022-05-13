fff5dc87aea46195b50c85168186f7f983ea3c60
Kapelu <ddaniel_calderon@hotmail.com>
2022-05-13 10:59:27 -0300

    Update 13/05/22

--- Modulo1-Foundations/03-EstructuraDeDatos-I/homework/homework.js
+++ Modulo1-Foundations/03-EstructuraDeDatos-I/homework/homework.js
@@ -7,9 +7,7 @@ nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número 
 
 nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
 Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
-
 Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
-
 
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
@@ -25,7 +23,6 @@ Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde e
   - enqueue: agrega un valor respetando el orden.
   - dequeue: remueve un valor respetando el orden. Retorna undefined cuando la queue está vacía.
   - size: retorna el tamaño (cantidad de elementos) de la queue.
-
 Pueden utilizar class o función constructora.
 */
 
@@ -33,9 +30,11 @@ function Queue() {
 
 }
 
+
+
+
 // No modifiquen nada debajo de esta linea
 // --------------------------------
-
 module.exports = {
   Queue,
   nFactorial,
