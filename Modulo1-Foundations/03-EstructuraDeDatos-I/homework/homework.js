190c7798237a24e0835f3c2ac321e0fcb1de56dd
Kapelu <ddaniel_calderon@hotmail.com>
2022-05-13 23:20:51 -0300

    Update 13/05/22

--- Modulo1-Foundations/03-EstructuraDeDatos-I/homework/homework.js
+++ Modulo1-Foundations/03-EstructuraDeDatos-I/homework/homework.js
@@ -5,17 +5,39 @@ Definir las funciones recursivas nFactorial y nFibonacci.
 
 nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1
 
-nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
-Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
-Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
 
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 
 function nFactorial(n) {
+  // devolvé el factorial de n (n!)
+  // ej:
+  // el factorial de 3 es 6 (3 * 2 * 1)
+  if (n >= 0 && n < 2) return 1;
+  
+  // ALTERNATIVA
+  // if(n===0 || n===1) return 1;
+  
+  return n * nFactorial(n - 1);
 }
-
+/*
+nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
+Ejemplo: nFibonacci(7) retornará 13, ya que 13 es el dígito que está en la posición 7 de la secuencia.
+Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+*/
 function nFibonacci(n) {
+  // Secuencia de Fibonacci: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,…
+  // Retorna el enésimo numero de la serie
+  // nFibonacci(0) // 0  // el elemento 0 es cero
+  // nFibonacci(1) // 1 // el elemento 1 es 1
+  // nFibonacci(6) // 1 // el elemento 6 es 8
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  // ALTERNATIVA
+  // if(n>=0 && n<2) return n;
+
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 }
 
 /*
