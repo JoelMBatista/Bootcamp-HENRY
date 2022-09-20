302ac2fc3d81e4f9c0a4b189b9f2783fe27dccee
Kapelu <ddaniel_calderon@hotmail.com>
2022-09-20 18:40:55 -0300

    UpDate 20/09/22

--- Modulo1-Foundations/03-EstructuraDeDatos-I/homework/homework.js
+++ Modulo1-Foundations/03-EstructuraDeDatos-I/homework/homework.js
@@ -4,10 +4,9 @@
 nFactorial(n) debe retornar el factorial de n sabiendo que, siendo n un número natural, su factorial (representado como n!) es el producto de n por todos los números naturales menores que él y mayores a 0. Ejemplo: 5! = 5 * 4 * 3 * 2 * 1 = 120
 */
 function nFactorial(n) {
-  let factorial = (n <=1) ? 1 : n * nFactorial( n -1)
-  return factorial
+  // Tú código acá
 }
-console.log(nFactorial(5))
+
 
 /*
 nFibonacci(n) debe retornar el enésimo número de la secuencia de Fibonacci, tomando al 0 y al 1, respectivamente, como primer y segundo elementos de la misma, y sabiendo que cualquier elemento que se agregue a esta secuencia será el resultado de la suma del último elemento y el anterior.
@@ -16,10 +15,8 @@ Secuencia:  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 Como ejercicio adicional y completamente opcional, al terminar de resolver este problema pueden intentar definir funciones que logren los mismos resultados pero de manera iterativa.
 */
 function nFibonacci(n) {
-  let fibonacci = (n === 0 || n=== 1) ? n : nFibonacci(n-1) + nFibonacci (n-2)
-  return fibonacci
+  // Tú código acá
 }
-console.log(nFibonacci(7))
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -30,7 +27,7 @@ Pueden utilizar class o función constructora.
 */
 
 function Queue() {
-  this.array = [];
+  this.array = []
 }
 
 Queue.prototype.enqueue = function (elemento) {
