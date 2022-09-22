1014e76aa96cf51b47243e893812f9bf22c46d4d
Kapelu <ddaniel_calderon@hotmail.com>
2022-09-21 23:10:03 -0300

    UpDate 21/09/22

--- Modulo1-Foundations/00-IntroToCS/homework/homework.js
+++ Modulo1-Foundations/00-IntroToCS/homework/homework.js
@@ -1,15 +1,30 @@
 'use strict'
-
-function BinarioADecimal(num) {
-  // Tú código acá
+/**
+ * 
+ * @binarioAdecimal {10101}  / De derecha a izquierda
+ *    1 = 1 x 2^0 = 1 x 0 =  1 
+ *    0 = 0 x 2^1 = 0 x 2 =  0
+ *    1 = 1 x 2^2 = 1 x 4 =  4  = 21
+ *    0 = 0 x 2^3 = 0 x 8 =  0
+ *    1 = 1 x 2^4 = 1 x16 = 16
+ * 
+ *     Decimal = arg + 
+ */
+function binarioAdecimal(num) {
+  let decimal = 0
+  for (let i = 0; i < num.length; i++) {
+    decimal = decimal + num[i] * Math.pow(2, num.length - 1 - i)
+  }
+  return decimal
 }
 
-function DecimalABinario(num) {
+
+function decimalAbinario(num) {
   // Tú código acá
 }
 
 
 module.exports = {
-  BinarioADecimal,
-  DecimalABinario,
+  binarioAdecimal,
+  decimalAbinario,
 }
--- Modulo1-Foundations/00-IntroToCS/homework/tests/CS.test.js
+++ Modulo1-Foundations/00-IntroToCS/homework/tests/CS.test.js
@@ -1,23 +1,23 @@
 /* eslint-disable no-undef */
 const {
-  BinarioADecimal,
-  DecimalABinario,
+  binarioAdecimal,
+  decimalAbinario,
 } = require('../homework.js');
 
-describe('BinarioADecimal(num)', function() {
+describe('binarioAdecimal(num)', function() {
   it('should return 2', function() {
-    expect(BinarioADecimal('10')).toBe(2);
+    expect(binarioAdecimal('10')).toBe(2);
   });
   it('should return 7', function() {
-    expect(BinarioADecimal('111')).toBe(7);
+    expect(binarioAdecimal('111')).toBe(7);
   });
 });
 
-describe('DecimalABinario(num)', function() {
+describe('decimalAbinario(num)', function() {
   it('should return "100"', function() {
-    expect(DecimalABinario(4)).toBe('100');
+    expect(decimalAbinario(4)).toBe('100');
   });
   it('should return "111"', function() {
-    expect(DecimalABinario(7)).toBe('111');
+    expect(decimalAbinario(7)).toBe('111');
   });
 });
